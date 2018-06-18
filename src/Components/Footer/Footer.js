cd67e50bde7755ad84e535549c13307acddc264b
Timothy Brewster <trbrwstr@protonmail.com>
2018-06-18 12:50:24 -0700

    You have terrible commit messages

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import IconImage from "../IconImage";
+
+const Footer = () =>
+
+    <footer className="footer">
+        <div className="container">
+            <p className="text-muted text-center">TRBRWSTR 2018</p>
+        </div>
+    </footer>
+
+export default Footer;
