fa56f7451fc3cc0cc185338075cd7fb585efb8e1
Timothy Brewster <trbrwstr@protonmail.com>
2018-06-18 19:06:15 -0700

    Little of this. Little of that

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -17,10 +17,10 @@ class App extends Component {
                 <hr />
                 <div className="row">
                     <div className="col contactCol">
-                        <Card source={require("./images/gmail.png")} text={<a href="trbrwstr@gmail.com" target="_blank"><button className="btn btn-dark">Email</button></a>} />
                         <Card source={require("./images/github.png")} text={<a href="https://github.com/trbrwstr" target="_blank"><button className="btn btn-dark">GitHub</button></a>} />
                         <Card source={require("./images/linkedin.png")} text={<a href="https://www.linkedin.com/in/trbrwstr" target="_blank"><button className="btn btn-dark">LinkedIn</button></a>} />
-                        <Card source={require("./images/phone.png")} text={<a href="619-348-4424"><button className="btn btn-dark">619-348-4424</button></a>} />
+                        <Card source={require("./images/phone.png")} text={<button className="btn btn-dark">619-348-4424</button>} />
+                        <Card source={require("./images/gmail.png")} text={<button className="btn btn-dark">trbrwstr@gmail.com</button>} />
                     </div>
                 </div>
                 <Footer />
