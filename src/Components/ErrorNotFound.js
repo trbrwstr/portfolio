ef32a28379b763e20b03ee1fd734c344d41ff370
trbrwstr <trbrwstr@gmail.com>
2019-05-12 19:01:36 -1000

    Update ErrorNotFound.js

--- src/Components/ErrorNotFound.js
+++ src/Components/ErrorNotFound.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
+import Navbar from "../Navbar";
+import Jumbotron from './Jumbotron';
+
 
 export default class ErrorNotFound extends Component {
         render() {
             return (
-                <div id="error">
-                    <h1 className="notFoundTitle">404 Page Not Found</h1>
-                    <p className="notFoundDesc">Nothing to see here. Move Along
-                    </p>
-                </div>
-            );
-        }
-
+            <div id='error'>
+                <Navbar />
+                <Jumbotron phrase="404 Page Not Found" subtitle="Not the droid you're looking for. Move along." />
+            </div>
+        );          
+    }
 }
